c4c5f5b9a1864c41747670cb63545667559c7a78
Haalu-Kai <tinavie00@gmail.com>
2026-04-14 09:40:46 +0300

    Update test_order_creation.py

--- test_order_creation.py
+++ test_order_creation.py
@@ -1,20 +1,23 @@
 import requests
 import pytest
+import allure
 from data import BASE_URL, ORDER_PAYLOAD, COLORS
 
 
-@pytest.mark.parametrize("color", COLORS)
-def test_create_order_with_different_colors(color):
-    """Проверка создания заказа с разными вариантами цвета:
-    - один цвет (BLACK / GREY)
-    - оба цвета
-    - без цвета
-    """
-    payload = ORDER_PAYLOAD.copy()
-    payload["color"] = color
+@allure.feature("Работа с заказами")
+@allure.story("Создание заказа")
+class TestOrderCreation:
 
-    response = requests.post(f"{BASE_URL}/orders", json=payload)
+    @allure.title("Создание заказа с разными цветами: {color}")
+    @allure.description("Проверка создания заказа при разных вариантах поля color (один цвет, два цвета, без цвета)")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.parametrize("color", COLORS)
+    def test_create_order_with_different_colors(self, color):
+        payload = ORDER_PAYLOAD.copy()
+        payload["color"] = color
 
-    assert response.status_code == 201
-    assert "track" in response.json()
-    assert isinstance(response.json()["track"], int)
+        response = requests.post(f"{BASE_URL}/orders", json=payload)
+
+        assert response.status_code == 201
+        assert "track" in response.json()
+        assert isinstance(response.json()["track"], int)
